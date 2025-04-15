0c7262a45d47d89f64beb80a84b1d39c78ac4abf
Kogaraashi1 <sexycamila276@gmail.com>
2025-04-15 12:50:00 -0500

    Key maps and cmd

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,11 @@
 return {
 
 	"tpope/vim-fugitive",
+	cmd = {"G", "Git"},
+	keys = {
+	  {"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
+	  {"<leader>gr", ":Git pull<cr>", desc = "Git pull"},
+	  
+	},
 
 }
